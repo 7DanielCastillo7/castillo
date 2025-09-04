58cd15e4722e0df16e451c1a873286651ebf79c1
DanielCastillo <daniel.castillo-l@escuelaing.edu.co>
2025-09-03 21:07:29 -0500

    main

---
+++ zzzzz.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+float numDec=3.14;
+void func(){
+    numDec=3.14156;
+    std::cout<<"Global variable: "<<numDec<<std::endl;
+    //std::cout<<"main func variable: "<<a<<std::endl;
+}
+
+int main(){
+    /*Multi
+    line
+    comment
+    */
+   int a=1;
+   std::cout<<"Global variable:"<<numDec<<std::endl;
+    std::cout<<"main func variable: "<<a<<std::endl;
+    func();
+    return 0;
+}
