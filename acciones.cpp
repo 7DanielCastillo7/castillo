d2eff6b614e8dc42ce2698c551086c3ca173f841
DanielCastillo <daniel.castillo-l@escuelaing.edu.co>
2025-10-15 12:19:31 -0500

    Acciones

---
+++ acciones.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ganancia=0;
+        for (int i=0; i<prices.size(); i++){
+            for (int j=i+1; j<prices.size(); j++){
+                if (prices[j] - prices[i]> ganancia){  
+                    ganancia= prices[j]-prices[i];
+
+                }
+            }
+        }
+    return ganancia;    
+    }
+};
+  
+int main(){
+    int dias;
+    cout<< "¿cuantos dias de precios hay? ";
+    cin >> dias; 
+    vector<int> prices(dias);
+    for (int i=0; i<dias; i++){
+        cout << "Precio dia " << i+1 << ": ";
+        cin >> prices[i]; 
+    }
+    Solution acciones;
+    int ganancia = acciones.maxProfit(prices);  
+    cout << "La ganancia maxima es: " << ganancia << endl;
+  
+   
+    return 0;
+}
